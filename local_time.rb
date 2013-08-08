1fcea9fb758fd54c081b4cc38039fe8735ae4461
ISHIOKA Sho <ishioka@jupiter.desc.okayama-u.ac.jp>
2013-08-08 10:30:30 +0900

    edit local_time.rb

--- local_time.rb
+++ local_time.rb
@@ -26,14 +26,15 @@ def local_time(var_name,list)
       hr_in_day = gpopen(rst_file,'time').get_att("hour_in_day")
     rescue
       hr_in_day = 24 / omega_ratio(list.name[n])
+      hr_in_day = 24 if list.id.include?("coriolis")
     end
 
     lon = gp.axis('lon')
 
     local = lon.pos
     local = lon.pos*hr_in_day/360
-    local.name = "local"
-    local.name = "hrs"
+    local.long_name = "local time"
+#    local.name = "hrs"
 
     lon = lon.to_gphys
     local_time = lon.copy
@@ -52,10 +53,10 @@ def local_time(var_name,list)
       # 日付が変わる経度を検出
       local_time = nowtime + lon/360
       local_time = (local_time - local_time.to_i)*hr_in_day
-      local_min_index = local.val.to_a.index(local.val.min)
+      local_min_index = local_time.val.to_a.index(local_time.val.min)
       # データの並び替え
-      gp_local[0..local_min_index-nlon-1,false] = gphys[local_min_index..-1,false]
-      gp_local[local_index_min-nlon..-1,false] = gphys[0..local_min_index-1,false]
+      gp_local[0..nlon-1-local_min_index,false] = gphys[local_min_index..-1,false]
+      gp_local[nlon-local_min_index..-1,false] = gphys[0..local_min_index-1,false]
       # lon -> localtime 変換
       gp_local.axis("lon").set_pos(local)
       [gp_local]
@@ -67,68 +68,119 @@ end
 def local_time_mean(var_name,list)
   list.dir.each_index do |n|
     begin 
-      gp = gpopen(list.dir[n] + var_name + '.nc',var_name)
-      time = gpopen(list.dir[n] + var_name + '.nc','time')
+      gp = GPhys::IO.open(list.dir[n] + var_name + '.nc',var_name)
     rescue
       print "[#{var_name}](#{list.dir[n]}) is not exist\n"
       next
     end
-
-    # hr_in_day の取得
-    if time.get_att("hour_in_day") != nil then
-      hr_in_day = time.get_att("hour_in_day")
-    else
+ 
+    begin
+      rst_file = list.dir[n].sub("data/","rst_data/rst1800.nc").sub("data1400/","rst_data/rst1440.nc")
+      hr_in_day = gpopen(rst_file,'time').get_att("hour_in_day")
+    rescue
       hr_in_day = 24 / omega_ratio(list.name[n])
     end
 
-    # 時間の単位を hrs に合わせる
-    if time.units.to_s=='min' 
-      time = time / 60
-      time.units = 'hrs'
-    elsif time.units.to_s=='day'
-      time = time * 24
-      time.units = 'hrs'
-    end
-
     lon = gp.axis('lon')
-    local_time = lon.pos / 360 * hr_in_day
-    local_time.long_name = "local time"
-    local_time.units = "hrs"
+
+    local = lon.pos
+    local = lon.pos*hr_in_day/360
+    local.name = "local"
+    local.name = "hrs"
 
     lon = lon.to_gphys
-    dlon = lon[1].val-lon[0].val
-
+    local_time = lon.copy
+    nlon = lon.length
+    
     ave = 0
-    ofile = NetCDF.create(list.dir[n] + "MTlocal_" + var_name + '.nc')
-    GPhys.each_along_dims([gp,time], 'time') do |gphys,gtime|  
+    ofile = NetCDF.create(list.dir[n] + data_name + '.nc')
+    GPhys::NetCDF_IO.each_along_dims([gp], 'time') { 
+      |gphys|
+
+      nowtime = gphys.axis("time").to_gphys
       gp_local = gphys.copy
-      gp_local[false] = 0
-
-      hr = gtime.val/hr_in_day
-      hr = hr - hr.to_i
-
-      eqtime = 360*hr
-      local = lon + eqtime
-      for i in 0..local.length-1
-        if local[i].val > 360 then
-          local[i].val = local[i].val-360.0
-        end
-      end
-
-      min = local.val.min
-      for i in 0..lon.length-1
-        n = local.to_a.index(min + dlon*i)
-        gp_local[i,false].val = gphys[n,false].val
-      end
-      gp_local.axis("lon").set_pos(local_time)
+      # 時間の単位を[day]に変更
+      nowtime.val = nowtime.val/hr_in_day    if nowtime.units.to_s == "hrs"
+      nowtime.val = nowtime.val/hr_in_day/60 if nowtime.units.to_s == "min"
+      # 日付が変わる経度を検出
+      local_time = nowtime + lon/360
+      local_time = (local_time - local_time.to_i)*hr_in_day
+      local_min_index = local_time.val.to_a.index(local_time.val.min)
+      # データの並び替え
+      gp_local[0..nlon-1-local_min_index,false] = gphys[local_min_index..-1,false]
+      gp_local[nlon-local_min_index..-1,false] = gphys[0..local_min_index-1,false]
+      # lon -> localtime 変換
+      gp_local.axis("lon").set_pos(local)
       ave = ave + gp_local      
-    end
-    ave = ave[false,0]/time.length
+    }
+    ave = ave[false,0]/gp.axis("time").pos.length
+    data_name = 'MTlocal_' + var_name
     GPhys::IO.write(ofile, ave)
     ofile.close
   end
 end
 
+def local_time_mean_rank(var_name,list)
+  rank = ["rank000006.nc","rank000004.nc","rank000002.nc","rank000000.nc",
+          "rank000001.nc","rank000003.nc","rank000005.nc","rank000007.nc"]
+  list.dir.each_index do |n|
+    begin
+      rst_file = list.dir[n].sub("data/","rst_data/rst1800.nc").sub("data1400/","rst_data/rst1440.nc")
+      hr_in_day = gpopen(rst_file,'time').get_att("hour_in_day")
+    rescue
+      hr_in_day = 24 / omega_ratio(list.name[n])
+      hr_in_day = 24 if list.id.include?("coriolis")
+    end
+    str_add(list.dir[n]+var_name.sub(".nc","")+ "_",rank).each do |file|
+      begin 
+        gp = GPhys::IO.open(file,var_name)
+      rescue
+        p file 
+        print "[#{var_name}](#{list.dir[n]}) is not exist\n"
+        next
+      end
+  
+      lon = gp.axis('lon')
+  
+      local = lon.pos
+      local = lon.pos*hr_in_day/360
+      local.name = "local"
+      local.name = "hrs"
+  
+      lon = lon.to_gphys
+      local_time = lon.copy
+      nlon = lon.length
+      
+      ave = 0
+      GPhys.each_along_dims(gp,'time') do 
+        |gphys|
+  
+        nowtime = gphys.axis("time").to_gphys
+        gp_local = gphys.copy
+        # 時間の単位を[day]に変更
+        nowtime.val = nowtime.val/hr_in_day    if nowtime.units.to_s == "hrs"
+        nowtime.val = nowtime.val/hr_in_day/60 if nowtime.units.to_s == "min"
+        # 日付が変わる経度を検出
+        local_time.val = nowtime.val + lon.val/360
+        local_time.val = (local_time.val - local_time.val.to_i)*hr_in_day
+        local_min_index = local_time.val.to_a.index(local_time.val.min)
+        # データの並び替え
+        if local_min_index != 0 then
+          gp_local[0..nlon-1-local_min_index,false].val = gphys[local_min_index..-1,false].val
+          gp_local[nlon-local_min_index..-1,false].val = gphys[0..local_min_index-1,false].val
+        end
+        # lon -> localtime 変換
+        gp_local.axis("lon").set_pos(local)
+        ave = ave + gp_local      
+      end
+      ave = ave[false,0]/gp.axis("time").pos.length
+      ofile = NetCDF.create(file.sub(var_name,"MTlocal_" + var_name))
+      GPhys::IO.write(ofile, ave)
+      ofile.close
+    end
+  end
+end
+  
 def omega_ratio(name)# 名前解析 nameからomega/omega_Eを抽出
   if name[0..4] == "omega" or name[0..4] == "Omega" then
     var = name.sub("omega","").sub("Omega","")
@@ -153,29 +205,29 @@ varname = ARGV[1]
 local_time(varname,list) if varname != nil
 #local_time("OSRA",list)
 #=begin
-local_time_mean('Rain',list)
-local_time_mean('RainCumulus',list)
-local_time_mean('RainLsc',list)
-local_time_mean('EvapA',list)
-local_time_mean('SensA',list)
-local_time_mean('SSRA',list)
-local_time_mean('SLRA',list)
-local_time_mean('OSRA',list)
-local_time_mean('OLRA',list)
-local_time_mean('SurfTemp',list)
-local_time_mean('Temp',list)
+local_time_mean_rank('Rain',list)
+local_time_mean_rank('RainCumulus',list)
+local_time_mean_rank('RainLsc',list)
+local_time_mean_rank('EvapA',list)
+local_time_mean_rank('SensA',list)
+local_time_mean_rank('SSRA',list)
+local_time_mean_rank('SLRA',list)
+local_time_mean_rank('OSRA',list)
+local_time_mean_rank('OLRA',list)
+local_time_mean_rank('SurfTemp',list)
+local_time_mean_rank('Temp',list)
 #=end
-#local_time_mean('RH',list,name)
-local_time_mean("QVap",list)
-local_time_mean("SigDot",list)
-local_time_mean("U",list)
-local_time_mean("V",list)
-local_time_mean("RadLDWFLXA",list)
-local_time_mean("RadSDWFLXA",list)
-local_time_mean("RadLUWFLXA",list)
-local_time_mean("RadSUWFLXA",list)
+#local_time_mean_rank('RH',list,name)
+local_time_mean_rank("QVap",list)
+local_time_mean_rank("SigDot",list)
+local_time_mean_rank("U",list)
+local_time_mean_rank("V",list)
+local_time_mean_rank("RadLDWFLXA",list)
+local_time_mean_rank("RadSDWFLXA",list)
+local_time_mean_rank("RadLUWFLXA",list)
+local_time_mean_rank("RadSUWFLXA",list)
 
-local_time_mean("Ps",list)
+local_time_mean_rank("Ps",list)
 #local_time_mean("QVapCulumu",list)
 
 #local_time("DQVapDtCond",list)
