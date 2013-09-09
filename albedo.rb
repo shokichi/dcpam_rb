e4b787c4b6b33e7320949369ab8000fee1bf4d69
ISHIOKA Sho <ishioka@jupiter.desc.okayama-u.ac.jp>
2013-09-09 11:59:42 +0900

    commit check

--- albedo.rb
+++ albedo.rb
@@ -15,9 +15,9 @@ def albedo(dir,name)
   data_name = "Albedo"
   # file open
   osr = gpopen dir+"OSRA.nc"
-  return if osr.nil?
-
   tsw = gpopen dir+"RadSDWFLXA.nc"
+  return if osr.nil? or tsw.nil?
+
 
   if defined?(HrInDay) and !HrInDay.nil? then
     hr_in_day = HrInDay
@@ -25,36 +25,38 @@ def albedo(dir,name)
     hr_in_day = 24 / Utiles_spe.omega_ratio(name)
   end
 
-  if tsw.nil? then
-    nlon = osr.axis("lon").length
+  nlon = osr.axis("lon").length
+
+#  if tsw.nil? then
     # 太陽直下点の計算
 #    time = hrs2day(sw,hr_in_day).to_gphys.val[0]
 #    slon = (time - time.to_i)*360
-    slon = 0
-    slon = UNumeric[slon,"degree"]    # 太陽直下点経度
+#    slon = 0  #
+#    slon = UNumeric[slon,"degree"]    # 太陽直下点経度
 
     # 大気上端下向きのSW
-    tsw = osr[false,0].copy
-    tsw[false] = -1.0
-    tsw = tsw*SolarConst*((slon-tsw.axis("lon").to_gphys)*PI/180.0).cos
-    tsw = tsw*(tsw.axis("lat").to_gphys*PI/180.0).cos
-    tsw = tsw[nlon/4..nlon*3/4-1,false]
-  else
+#    tsw = osr[false,0].copy
+#    tsw[false] = -1.0
+#    tsw = tsw*SolarConst*((slon-tsw.axis("lon").to_gphys)*PI/180.0).cos
+#    tsw = tsw*(tsw.axis("lat").to_gphys*PI/180.0).cos
+  tsw = tsw[nlon/4..nlon*3/4-1,true,-1,true]
+#  else
 
-  end
+#  end
 
   # 計算
-  ofile = NetCDF.create( dir + data_name + '.nc')
-  GPhys::NetCDF_IO.each_along_dims_write(osr, ofile,'time') { 
-    |sw|
+  ofile = NetCDF.create(dir + "local_" + data_name + '.nc')
+  GPhys::NetCDF_IO.each_along_dims_write([osr,tsw], ofile,'time') { 
+    |sw,top|
 
     # OSRの地方時変換
     sw = local_time(sw,hr_in_day)
+    top = local_time(top,hr_in_day)
     sw = sw[nlon/4..nlon*3/4-1,false]
+    top = top[nlon/4..nlon*3/4-1,false]
     
     # albedo
-    albedo = sw.copy
-    albedo.val = 1.0 + sw.val/tsw.val.abs
+    albedo = 1.0 + sw/top
     albedo.name = data_name
     albedo.units = "1"
     albedo.long_name = "planetary albedo"
@@ -64,15 +66,6 @@ def albedo(dir,name)
   print "[#{data_name}](#{dir}) is created \n"
 end
 
-def hrs2day(gp,hr_in_day)
-  if gp.axis("time").pos.units.to_s != "hrs"
-#    print "Time units: "
-#    print gp.axis("time").units.to_s,"\n"
-    return gp.axis("time")
-  end
-  time = gp.axis("time").pos / hr_in_day
-  return gp.axis("time").set_pos(time) 
-end
 
 opt = OptionParser.new
 opt.on("-r","--rank") {Flag_rank = true}
