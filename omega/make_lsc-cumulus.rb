bac7e03abe9cf60d4e2e0356d7b5d85eac8d070d
ISHIOKA Sho <ishioka@jupiter.desc.okayama-u.ac.jp>
2013-09-18 15:33:33 +0900

    Add file

---
+++ omega/make_lsc-cumulus.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+# 
+#
+
+require 'numru/ggraph'
+require 'numru/gphys'
+require '/home/ishioka/ruby/dcpam_rb/lib/utiles_spe'
+include Utiles_spe
+include NumRu
+include Math
+
+def create_RainLsc(dir)
+  data_name = "RainLsc"
+  begin
+    gp = GPhys::IO.open(dir + 'DQVapDtLsc.nc',"DQVapDtLsc")
+    ps = GPhys::IO.open(dir + 'Ps.nc',"Ps")
+#  sig_weight = GPhys::IO.open(dir + 'DQVapDtLsc.nc',"sig_weight")
+    sig_weight = GPhys::IO.open('/home/ishioka/link/all/omega1/data/DQVapDtCumulus.nc',"sig_weight")
+  rescue
+    puts "No such file"
+    return
+  end
+  result = -LatentHeat * (gp * ps * sig_weight).sum("sig")/Grav
+  result.name = data_name
+  result.units = "W.m-2"
+  ofile = NetCDF.create(dir + data_name + '.nc')
+  GPhys::IO.write(ofile, result)
+  ofile.close
+  print "[#{data_name}](#{dir}) is created\n"
+end
+
+def create_RainCumulus(dir)
+  data_name = "RainCumulus"
+  begin
+    gp = GPhys::IO.open(dir + 'DQVapDtCumulus.nc','DQVapDtCumulus')
+    ps = GPhys::IO.open(dir + 'Ps.nc',"Ps")
+    sig_weight = GPhys::IO.open('/home/ishioka/link/all/omega1/data/DQVapDtCumulus.nc',"sig_weight")
+  rescue
+    puts "No such file"
+    return
+  end
+
+  result = -LatentHeat * (gp * ps * sig_weight).sum("sig")/Grav
+  result.name = data_name
+  result.units = "W.m-2"
+  ofile = NetCDF.create(dir + data_name + '.nc')
+  GPhys::IO.write(ofile, result)
+  ofile.close
+  print "[#{data_name}](#{dir}) is created\n"
+end
+
+list = Utiles_spe::Explist.new(ARGV[0])
+list.dir.each{|dir| create_RainLsc(dir)}
+list.dir.each{|dir| create_RainCumulus(dir)}
+
---
+++ omega/rain_rainc.rb
@@ -0,0 +1,54 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+# 
+#
+#
+
+require 'numru/ggraph'
+require 'numru/gphys'
+require File.expand_path(File.dirname(__FILE__)+"/utiles_omega.rb")
+require 'optparse'
+include Utiles_spe
+include NumRu
+include Math
+
+def rain_rainc(dir)
+  rain = gpopen dir + "Rain.nc"
+  rainc = gpopen dir + "RainCumulus.nc"
+  
+  return rainc/rain  
+end
+# ---------------------------------------
+
+
+
+opt = OptionParser.new
+opt.on("-r","--rank") {Flag_rank = true}
+opt.on("--ps") { IWS = 2}
+opt.on("--png") { 
+  DCL::swlset('lwnd',false)
+  IWS = 4
+}
+
+opt.parse!(ARGV)
+varname = VarName if defined?(VarName)
+list = Utiles_spe::Explist.new(ARGV[0])
+IWS = 1 if !defined?(IWS) or IWS.nil?
+
+# DCL set
+clrmp = 14  # カラーマップ
+DCL.sgscmn(clrmp)
+DCL.gropn(IWS)
+#DCL.sldiv('Y',2,1)
+DCL.sgpset('lcntl',true)
+DCL.sgpset('isub', 96)
+DCL.uzfact(1.0)
+
+list = Utiles_spe::Explist.new(ARGV[0])
+data = []
+list.dir.each{|dir| data << rain_rainc(dir)}
+Omega.lat_fig2(data,list,"min"=>0,"max"=>1)
+Omega.lonlat2(data,list,"min"=>0,"max"=>1)
+
+DCL.grcls
+rename_img_file(list,__FILE__)
