246070e6a4f0fb271be65f3d6a242a28220c8839
ISHIOKA Sho <ishioka@jupiter.desc.okayama-u.ac.jp>
2013-10-02 17:08:08 +0900

    Bug fix

--- omega/albedo_H2O.rb
+++ omega/albedo_H2O.rb
@@ -11,22 +11,12 @@ include Utiles_spe
 include NumRu
 include Math
 
-opt = OptionParser.new
-opt.on("-r","--rank") {Flag_rank = true}
-opt.on("-h VAL","--hr_in_day=VAL") {|hr_in_day| HrInDay = hr_in_day.to_i}
-opt.on("--ps") { IWS = 2}
-opt.on("--png") { 
-  DCL::swlset('lwnd',false)
-  IWS = 4
-}
 
 
 def draw_scatter(dir,name,hash={})
-  albedo = gpopen list.dir+"Albedo.nc"
-  h2o = gpopen list.dir+"H2O.nc"
-  albedo = cut_and_mean(albedo)
-  h2o = cut_and_mean(h2o)
-  
+  albedo = gpopen dir+"local_Albedo.nc","Albedo"
+  h2o = gpopen dir+"H2OLiq.nc"
+  return if albedo.nil? or h2o.nil?
 
   if defined?(HrInDay) and !HrInDay.nil? then
     hr_in_day = HrInDay
@@ -34,12 +24,14 @@ def draw_scatter(dir,name,hash={})
     hr_in_day = 24 / Utiles_spe.omega_ratio(name)
   end
 
-  skip = 6*6
-  (time.length/skip).times{ |t|
+  skip = 6*24
+  (albedo.axis("time").length/skip).times{ |t|
     time = t*skip 
-    x_coord = albedo[false,time]
-    y_coord = h2o[false,time]*cos_ang(h2o[false,time..time],hr_in_day)
-    if t == 0
+    albedo = albedo[false,time..time]
+    h2o = cut_and_mean(h2o[false,time..time],hr_in_day)
+    y_coord = albedo
+    x_coord = h2o/cos_ang(h2o,hr_in_day)
+    if t == 0 then
       GGraph.scatter(x_coord,y_coord,true,hash) 
     else  
       GGraph.scatter(x_coord,y_coord,false,hash)   
@@ -63,14 +55,39 @@ def cos_ang(gp,hr_in_day)
   ang.units = "1"
   ang = ang*((ang.axis("lon").to_gphys+slon)*PI/180.0).cos
   ang = ang*(ang.axis("lat").to_gphys*PI/180.0).cos
-  return ang
+  return ang + 1e-14
 end
 
-def cut_and_mean(gp)
+def cut_and_mean(gp,hr_in_day)
+  gp = local_time(gp,hr_in_day)
+  gp = gp[nlon/4+1..nlon*3/4-2,false]
   return gp
 end
+
+opt = OptionParser.new
+opt.on("-r","--rank") {Flag_rank = true}
+opt.on("-h VAL","--hr_in_day=VAL") {|hr_in_day| HrInDay = hr_in_day.to_i}
+opt.on("--ps") { IWS = 2}
+opt.on("--png") { 
+  DCL::swlset('lwnd',false)
+  IWS = 4
+}
+opt.parse!(ARGV)
+
+IWS = 1 if !defined?(IWS)
+# DCL set
+clrmp = 14  # カラーマップ
+DCL.sgscmn(clrmp)
+DCL.gropn(IWS)
+#DCL.sldiv('Y',2,1)
+DCL.sgpset('lcntl',true)
+DCL.sgpset('isub', 96)
+DCL.uzfact(1.0)
 
 
 list = Utiles_spe::Explist.new(ARGV[0])
 HrInDay = 24 if list.id.include?("coriolis")
-list.dir.each_index{ |n| draw_scatter(dir[n],name[n])}
+list.dir.each_index{ |n| draw_scatter(list.dir[n],list.name[n])}
+
+DCL.grcls
+rename_img_file(lists["all"].id,__FILE__)
