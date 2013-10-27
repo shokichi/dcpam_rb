192dc9ce29ae1b65f2a583f552878416feea264b
ISHIOKA Sho <eee.viajero730@gmail.com>
2013-10-27 09:20:55 +0900

    Sync

--- omega/make_lsc-cumulus.rb
+++ omega/make_lsc-cumulus.rb
@@ -6,12 +6,14 @@
 require 'numru/ggraph'
 require 'numru/gphys'
 require '/home/ishioka/ruby/dcpam_rb/lib/utiles_spe'
+require 'optparse'
 include Utiles_spe
 include NumRu
 include Math
 
 def create_RainLsc(dir)
   data_name = "RainLsc"
+  dir += PRF if defined?(PRF)
   begin
     gp = GPhys::IO.open(dir + 'DQVapDtLsc.nc',"DQVapDtLsc")
     ps = GPhys::IO.open(dir + 'Ps.nc',"Ps")
@@ -49,6 +51,11 @@ def create_RainCumulus(dir)
   ofile.close
   print "[#{data_name}](#{dir}) is created\n"
 end
+
+opt = OptionParser.new
+opt.on("-r","--rank") {Flag_rank = true}
+opt.on("--prefix==STR") {|str| PRF = str}
+opt.parse!(ARGV)
 
 list = Utiles_spe::Explist.new(ARGV[0])
 list.dir.each{|dir| create_RainLsc(dir)}
