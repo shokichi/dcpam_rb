ae3d4b99b40163c9934268d248ffb4372fe6cacd
ISHIOKA Sho <eee.viajero730@gmail.com>
2013-11-07 23:52:32 +0900

    change class gphys

--- lib/analy.rb
+++ lib/analy.rb
@@ -5,104 +5,176 @@
 require "numru/ggraph"
 require 'numru/gphys'
 require "./utiles_spe.rb"
+include Utiles_spe
 include NumRu
 include Math
 include NMath
-include Utiles_spe
 
 module Analy
-  def glmean(gp)  # 全球平均
-    cos_phi = ( gp.axis("lat").to_gphys * (Math::PI/180.0) ).cos
-    fact = cos_phi / cos_phi.mean
-    gp_mean = (gp * fact).mean("lon","lat")
-    return gp_mean
-  end
-  #---------------------- 
-  def latmean(gp)  # 南北平均
-    cos_phi = ( gp.axis("lat").to_gphys * (Math::PI/180.0) ).cos
-    fact = cos_phi / cos_phi.mean
-    gp_mean = (gp * fact).mean("lat")
-    return gp_mean
-  end
-  #------------------------
-  def std_error(narray,blocknum=nil) # 標準偏差
-    blocknum = narray.length if blocknum.nil?
-    ave = narray.mean
-    var = NArray.sfloat(blocknum)
-    range1 = 0
-    delrange = narray.length/blocknum
-    
-    for n in 0..blocknum-1 
-      range2 = range1 + delrange.to_i
-      var[n] = narray[range1..range2-1].mean
-      range1 = range2
+  class GPhys
+    def glmean  # 全球平均
+      gp = self.clone
+      cos_phi = ( gp.axis("lat").to_gphys * (Math::PI/180.0) ).cos
+      fact = cos_phi / cos_phi.mean
+      gp_mean = (gp * fact).mean("lon","lat")
+      return gp_mean
     end
-    
-    std = (((var-ave)**2).mean)**0.5
-    return std
-  end
-  
-  #---------------------- 
-  def virtical_integral(gp)  # 鉛直積分
-    begin
-      if gp.data.file.class == NArray then
-        sig_weight = gpopen(gp.data.file[0].path, "sig_weight")
-      else
-        sig_weight = gpopen(gp.data.file.path, "sig_weight")    
-      end
-
-    rescue
-
-      if gp.data.file == nil then
-        sig_weight = gpopen("./Temp.nc","sig_weight")
-      else
-        tmp = gp.data.file.path.split("/")
-        sig_weight = gpopen(gp.data.file.path.sub(tmp[-1],"Temp.nc"),"sig_weight")
-      end
+    #---------------------- 
+    def latmean  # 南北平均
+      gp = self.clone
+      cos_phi = ( gp.axis("lat").to_gphys * (Math::PI/180.0) ).cos
+      fact = cos_phi / cos_phi.mean
+      gp_mean = (gp * fact).mean("lat")
+      return gp_mean
     end
-
-    gp_vintg = (gp * sig_weight).sum("sig")
-    return gp_vintg
+    #---------------------- 
+    def virtical_integral  # 鉛直積分
+      gp = self.clone
+      begin
+        if gp.data.file.class == NArray then
+          sig_weight = gpopen(gp.data.file[0].path, "sig_weight")
+        else
+          sig_weight = gpopen(gp.data.file.path, "sig_weight")    
+        end
+        
+      rescue
+        
+        if gp.data.file == nil then
+          sig_weight = gpopen("./Temp.nc","sig_weight")
+        else
+          tmp = gp.data.file.path.split("/")
+          sig_weight = gpopen(gp.data.file.path.sub(tmp[-1],"Temp.nc"),"sig_weight")
+        end
+      end
+      
+      gp_vintg = (gp * sig_weight).sum("sig")
+      return gp_vintg
+    end
+    #----------------------
+    def intg_delpress(gp)  # 鉛直積分(気圧)
+      gp = self.clone
+      return gp
+      #    begin
+      #      if gp.data.file.class == NArray then
+      #        filename = gp.data.file[0].path
+      #      else
+      #        filename = gp.data.file.path
+      #      end
+      #      sig_weight = gpopen filename, "sig_weight"
+      #      ps = gpopen File.dirname(filename)+"/"+"Ps.nc"
+      #    rescue
+      #      if gp.data.file == nil then
+      #        sig_weight = gpopen "./Temp.nc","sig_weight"
+      #        ps = gpopen "./Ps.nc"
+      #      else
+      #        tmp = gp.data.file.path.split("/")
+      #        sig_weight = gpopen gp.data.file.path.sub(tmp[-1],"Temp.nc"),"sig_weight"
+      #        ps = gpopen gp.data.file.path.sub(tmp[-1],"Ps.nc")
+      #      end
+      #    end
+      #    gp_intg = (gp * ps * sig_weight).sum("sig")/Grav
+      #    return gp_intg
+    end
+    #----------------------
+    def wm2mmyr  # 降水量の単位変換(W.m-2 -> mm.yr-1)
+      gp = self.clone
+      return gp if gp.units.to_s.include?("W")
+      gp = gp*(3600 * 24 * 360) * 1000 / LatentHeat / WtWet 
+      gp.units = Units["mm.yr-1"]
+      return gp
+    end
+    #----------------------
+    def wm2mmhr  # 降水量の単位変換(W.m-2 -> mm.hr-1)
+      gp = self.clone
+      return gp if gp.units.to_s.include?("W")
+      gp = gp* 3600 * 1000 / LatentHeat / WtWet 
+      gp.units = Units["mm.hr-1"]
+      return gp
+    end
+    #-----------------------------------------  
+    def sig2press(ps) # 鉛直座標変換(sig -> press)
+      gp = self.clone
+      #  ps = gpopen(gp.data.file.path.sub(gp.name+".nc","Ps.nc"),"Ps")
+      # 座標データ取得
+      time = gp.axis(-1).to_gphys
+      sig = gp.axis(-2).to_gphys
+      
+      
+      # 気圧データの準備
+      press = gp.copy
+      press.units = 'Pa'
+      press.name = "press"
+      press.long_name = "pressure"
+      press[false] = 0
+      for k in 0..sig.length-1
+        press[false,k,true] = ps * sig[k].val
+      end
+      
+      # 補助座標に気圧を設定 
+      gp.set_assoc_coords([press])
+      
+      # 気圧座標の値を準備
+      press_crd = VArray.new( sig.val*RefPrs, {"units"=>"Pa"}, "press")
+      
+      # 鉛直座標を気圧に変換
+      gp_press = gp.interpolate(sig.name=>press_crd)
+      
+      return gp_press
+    end
+    # ---------------------------------------
+    def day2hrs(gp,hr_in_day)
+      gp = self.clone
+      time =  gp.axis("time").pos * hour_in_day
+      gp.axis("time").set_pos(time)  
+      return gp
+    end
+    #----------------------------------------
+    def hrs2day(hr_in_day)
+      gp = self.clone
+      time =  gp.axis("time").pos / hr_in_day
+      gp.axis("time").set_pos(time)  
+      return gp
+    end
+    #----------------------------------------
+    def min2day(gp,hr_in_day)
+      time =  gp.axis("time").pos / hr_in_day / 60
+      gp.axis("time").set_pos(time)
+      return gp
+    end
   end
-  #----------------------
-  def intg_delpress(gp)  # 鉛直積分(気圧)
-    return gp
-#    begin
-#      if gp.data.file.class == NArray then
-#        filename = gp.data.file[0].path
-#      else
-#        filename = gp.data.file.path
-#      end
-#      sig_weight = gpopen filename, "sig_weight"
-#      ps = gpopen File.dirname(filename)+"/"+"Ps.nc"
-#    rescue
-#      if gp.data.file == nil then
-#        sig_weight = gpopen "./Temp.nc","sig_weight"
-#        ps = gpopen "./Ps.nc"
-#      else
-#        tmp = gp.data.file.path.split("/")
-#        sig_weight = gpopen gp.data.file.path.sub(tmp[-1],"Temp.nc"),"sig_weight"
-#        ps = gpopen gp.data.file.path.sub(tmp[-1],"Ps.nc")
-#      end
-#    end
-#    gp_intg = (gp * ps * sig_weight).sum("sig")/Grav
-#    return gp_intg
+  #-----------------------------------------  
+  def local_time(gphys,hr_in_day)
+      
+    lon = gphys.axis('lon')
+    local = lon.pos
+    local = lon.pos*hr_in_day/360
+    local.long_name = "local time"
+    
+    lon = lon.to_gphys
+    local_time = lon.copy
+    nlon = lon.length
+    
+    gp_local_converted = GPhys.each_along_dims(gphys,"time"){ |gp|
+      nowtime = gp.axis("time").to_gphys
+      gp_local = gp.copy
+      # 時間の単位を[day]に変更
+      nowtime.val = nowtime.val/hr_in_day    if nowtime.units.to_s == "hrs"
+      nowtime.val = nowtime.val/hr_in_day/60 if nowtime.units.to_s == "min"
+      # 日付が変わる経度を検出
+      local_time.val = nowtime.val + lon.val/360
+      local_time.val = (local_time.val - local_time.val.to_i)*hr_in_day
+      local_min_index = local_time.val.to_a.index(local_time.val.min)
+      # データの並び替え
+      if local_min_index != 0 then
+        gp_local[0..nlon-1-local_min_index,false].val = gp[local_min_index..-1,false].val
+        gp_local[nlon-local_min_index..-1,false].val = gp[0..local_min_index-1,false].val
+      end
+      # lon -> localtime 変換
+      gp_local.axis("lon").set_pos(local)
+      return gp_local 
+    }  
+    return gp_local_converted
   end
-  #----------------------
-  def self.wm2mmyr(gp)  # 降水量の単位変換(W.m-2 -> mm.yr-1)
-    return gp if gp.units.to_s.include?("W")
-    gp = gp*(3600 * 24 * 360) * 1000 / LatentHeat / WtWet 
-    gp.units = Units["mm.yr-1"]
-    return gp
-  end
-  #----------------------
-  def self.wm2mmhr(gp)  # 降水量の単位変換(W.m-2 -> mm.hr-1)
-    return gp if gp.units.to_s.include?("W")
-    gp = gp* 3600 * 1000 / LatentHeat / WtWet 
-    gp.units = Units["mm.hr-1"]
-    return gp
-  end
-  
   #-----------------------
   def calc_press(ps,sig)
     lon = ps.axis(0)
@@ -119,7 +191,6 @@ module Analy
     press = press * sig
     return press
   end
-  
   #----------------------------------------- 
   def sig2press_save(dir,var_name) # 鉛直座標変換(sig -> press)
     # ファイルオープン
@@ -159,37 +230,6 @@ module Analy
     }
     
   end
-  
-  #-----------------------------------------  
-  def self.sig2press(gp,ps) # 鉛直座標変換(sig -> press)
-    #  ps = gpopen(gp.data.file.path.sub(gp.name+".nc","Ps.nc"),"Ps")
-    # 座標データ取得
-    time = gp.axis(-1).to_gphys
-    sig = gp.axis(-2).to_gphys
-    
-    
-    # 気圧データの準備
-    press = gp.copy
-    press.units = 'Pa'
-    press.name = "press"
-    press.long_name = "pressure"
-    press[false] = 0
-    for k in 0..sig.length-1
-      press[false,k,true] = ps * sig[k].val
-    end
-    
-    # 補助座標に気圧を設定 
-    gp.set_assoc_coords([press])
-    
-    # 気圧座標の値を準備
-    press_crd = VArray.new( sig.val*RefPrs, {"units"=>"Pa"}, "press")
-    
-    # 鉛直座標を気圧に変換
-    gp_press = gp.interpolate(sig.name=>press_crd)
-    
-    return gp_press
-  end
-  
   #----------------------------------------- 
   def self.calc_msf(gv,gps,sigm)  # 質量流線関数の計算
     # file open
@@ -327,41 +367,7 @@ module Analy
     }
     ofile.close
     print "[#{data_name}](#{dir}) is created\n"
-  end
-  # ---------------------------------------
-  def local_time(gphys,hr_in_day)
-    
-    lon = gphys.axis('lon')
-    local = lon.pos
-    local = lon.pos*hr_in_day/360
-    local.long_name = "local time"
-    
-    lon = lon.to_gphys
-    local_time = lon.copy
-    nlon = lon.length
-    
-    gp_local_converted = GPhys.each_along_dims(gphys,"time"){ |gp|
-      nowtime = gp.axis("time").to_gphys
-      gp_local = gp.copy
-      # 時間の単位を[day]に変更
-      nowtime.val = nowtime.val/hr_in_day    if nowtime.units.to_s == "hrs"
-      nowtime.val = nowtime.val/hr_in_day/60 if nowtime.units.to_s == "min"
-      # 日付が変わる経度を検出
-      local_time.val = nowtime.val + lon.val/360
-      local_time.val = (local_time.val - local_time.val.to_i)*hr_in_day
-      local_min_index = local_time.val.to_a.index(local_time.val.min)
-      # データの並び替え
-      if local_min_index != 0 then
-        gp_local[0..nlon-1-local_min_index,false].val = gp[local_min_index..-1,false].val
-        gp_local[nlon-local_min_index..-1,false].val = gp[0..local_min_index-1,false].val
-      end
-      # lon -> localtime 変換
-      gp_local.axis("lon").set_pos(local)
-      return gp_local 
-    }  
-    return gp_local_converted
-  end
-  
+  end  
   # ---------------------------------------
   def omega_ratio(name)# 名前解析 nameからomega/omega_Eを抽出
     if name[0..4] == "omega" or name[0..4] == "Omega" then
@@ -380,25 +386,6 @@ module Analy
     end
     return ratio
   end
-  # ---------------------------------------
-  def day2hrs(gp,name)
-    hour_in_day = 24 / omega_ratio(name)
-    time =  gp.axis("time").pos * hour_in_day
-    gp.axis("time").set_pos(time)  
-    return gp
-  end
-  #----------------------------------------
-  def hrs2day(gp,hr_in_day)
-    time =  gp.axis("time").pos / hr_in_day
-    gp.axis("time").set_pos(time)  
-    return gp
-  end
-  #----------------------------------------
-  def min2day(gp,hr_in_day)
-    time =  gp.axis("time").pos / hr_in_day / 60
-    gp.axis("time").set_pos(time)
-    return gp
-  end
   #----------------------------------------
   def thinout(gp,delnum)
     time = 0
@@ -407,5 +394,22 @@ module Analy
       time += 1
     }
     result
+  end
+  #------------------------
+  def std_error(narray,blocknum=nil) # 標準偏差
+    blocknum = narray.length if blocknum.nil?
+    ave = narray.mean
+    var = NArray.sfloat(blocknum)
+    range1 = 0
+    delrange = narray.length/blocknum
+    
+    for n in 0..blocknum-1 
+      range2 = range1 + delrange.to_i
+      var[n] = narray[range1..range2-1].mean
+      range1 = range2
+    end
+    
+    std = (((var-ave)**2).mean)**0.5
+    return std
   end
 end
