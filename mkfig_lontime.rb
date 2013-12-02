f52dab1572b0896281f08b840661397a44a39118
ISHIOKA Sho <eee.viajero730@gmail.com>
2013-12-02 22:44:33 +0900

    Add file

---
+++ mkfig_lontime.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+# standerd figure
+# ホフメラーダイアグラム
+
+# 
+require "numru/ggraph"
+require File.expand_path(File.dirname(__FILE__)+"/lib/make_figure.rb")
+require 'optparse'
+include MKfig
+include NumRu
+
+# option
+opt = OptionParser.new
+opt.on("-r","--rank") {Flag_rank = true}
+opt.on("-n VAR","--name=VAR") {|name| VarName = name}
+opt.on("--lat=Lat") {|lat| Lat = lat.to_f}
+opt.on("--max=max") {|max| Max = max.to_f}
+opt.on("--min=min") {|min| Min = min.to_f}
+opt.on("--nlev=nlev") {|nlev| Nlev = nlev.to_f}
+opt.on("--ps") { IWS = 2}
+opt.on("--png") { 
+  DCL::swlset('lwnd',false)
+  IWS = 4
+}
+opt.parse!(ARGV)
+
+varname = VarName if defined?(VarName)
+list = Utiles_spe::Explist.new(ARGV[0])
+IWS = 1 if !defined?(IWS) or IWS.nil?
+
+
+# DCL set
+clrmp = 14  # カラーマップ
+DCL.sgscmn(clrmp)
+DCL.gropn(IWS)
+#DCL.sldiv('Y',2,1)
+DCL.sgpset('lcntl',true)
+DCL.sgpset('isub', 96)
+DCL.uzfact(1.0)
+
+if !varname.nil? then
+  figopt = set_figopt
+  lontime(varname,list,figopt)
+else
+  lontime("Rain",list,figopt)
+end
+
+DCL.grcls
+rename_img_file(list,__FILE__)
