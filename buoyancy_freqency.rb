67a9250b7e8a0bf136a85c5eec2dca027aa83005
ISHIOKA Sho <eee.viajero730@gmail.com>
2013-12-17 12:58:10 +0900

    Add create_DThetaDsigMoist
    Add method for calculation of equivalent poteitial temperature.

--- buoyancy_freqency.rb
+++ buoyancy_freqency.rb
@@ -30,6 +30,13 @@ def calc_DThetaDsig(z_temp,ps,sig,sigm)
   return result
 end
 
+def calc_DThetaDsigMoist(z_temp,qvap,ps,sig,sigm)
+  z_press = calc_press(ps,sig)
+  z_theta = equiv_potential_temperature(z_temp,qvap,z_press)
+  result = - diff_sig(z_theta,sigm)
+  return result
+end
+
 def create_BVfreq(list)
   list.dir.each_index do |n|
     data_name = 'BVfreq'
@@ -65,6 +72,43 @@ def create_BVfreq(list)
   end
 end
 
+def create_DThetaDsigMoist(list)
+  list.dir.each_index do |n|
+    data_name = 'DThetaDsigMoist'
+    data_long_name = "vertical grad of equiv potential temp"
+    temp = gpopen list.dir[n] + "Temp.nc"
+    temp = gpopen list.dir[n] + "QVap.nc"
+    sigm = gpopen list.dir[n] + "Temp.nc","sigm"  
+    ps = gpopen list.dir[n] + "Ps.nc"
+    return if temp.nil? or ps.nil?  
+    sig = temp.axis("sig").to_gphys
+    
+    if defined? HrInDay  and HrInDay.nil? then
+      hr_in_day = HrInDay 
+    elsif list.id.include? "coriolis"
+      hr_in_day = 24
+    else
+      hr_in_day = 24 / omega_ratio(list.name[n])
+    end
+    
+    ave = 0
+    GPhys.each_along_dims([temp,qvap,ps],'time') do
+      |gtemp,gqvap,gps|
+      dthetadz = calc_DThetaDsig(gtemp,gqvap,gps,sig,sigm)
+      ave += local_time(dthetadz,hr_in_day)
+    end
+    ave.name = data_name
+    ave.long_name = data_long_name    
+    ave = ave[false,0] if ave.axnames.include?("time")
+    ave = ave/temp.axis("time").pos.length
+    ofile = NetCDF.create(list.dir[n]+"MTlocal_"+data_name+".nc")
+    GPhys::IO.write(ofile, ave)
+    ofile.close
+    print "[#{data_name}](#{list.dir[n]}) is created \n"
+  end
+end
+
+
 def create_DThetaDsig(list)
   list.dir.each_index do |n|
     data_name = 'DThetaDsig'
@@ -86,7 +130,7 @@ def create_DThetaDsig(list)
     ave = 0
     GPhys.each_along_dims([temp,ps],'time') do
       |gtemp,gps|
-      dthetadz = calc_DThetaDsig(gtemp,gps,sig,sigm)
+      dthetadz = calc_DThetaDsigMoist(gtemp,gps,sig,sigm)
       ave += local_time(dthetadz,hr_in_day)
     end
     ave.name = data_name
@@ -103,7 +147,14 @@ end
 opt = OptionParser.new
 opt.on("-r","--rank") {Flag_rank = true}
 opt.on("-h VAL","--hr_in_day=VAL") {|hr_in_day| HrInDay = hr_in_day.to_i}
+opt.on("--bf") {CalcBF = true}
+opt.on("--moist") {CalcEQV = true}
 opt.parse!(ARGV)
 
-#create_BVfreq(Explist.new(ARGV[0]))
-create_DThetaDsig(Explist.new(ARGV[0]))
+if defined? CalcBF
+  create_BVfreq(Explist.new(ARGV[0]))
+elsif defined? CalcEQV
+  create_DThetaDsigSat(Explist.new(ARGV[0]))
+else
+  create_DThetaDsigSat(Explist.new(ARGV[0]))
+end
