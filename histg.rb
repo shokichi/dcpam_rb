292a8d41f04703e03865035cddb3d98d7efa620d
ISHIOKA Sho <eee.viajero730@gmail.com>
2013-12-31 16:26:36 +0900

    Golf code

--- histg.rb
+++ histg.rb
@@ -4,98 +4,74 @@
 # ヒストグラムの作成 
 #
 
-require 'numru/ggraph'
-require 'numru/gphys'
 require 'numru/ganalysis'
-require File.expand_path(File.dirname(__FILE__)+"/"+"lib/utiles_spe.rb")
-require 'narray'
-include Utiles_spe
+require File.expand_path(File.dirname(__FILE__)+"/"+"lib/make_fig.rb")
+include MKfig
 include NumRu
 include Math
 
-def hist_fig(data_name,min,max,nbins,dir,name)
-  for n in 0..dir.length-1
-    begin
-      gp = GPhys::IO.open(dir[n] + data_name + ".nc",data_name)
-    rescue
-      print "[#{data_name}](#{dir[n]}) is not exist\n"
-      next
-    end
-    if data_name[0..3]=="Rain" then
-      if gp.units.to_s != "mm.h-1" then
-        # 降水量の単位変換
-      end
-    end
-    gh = histogram_div(gp.cut("sig"=>1),min,max,nbins)
-    if n == 0 
-      lc = 13
-      vx = 0.82
-      vy = 0.8
-      GGraph.line(gh, true, "title"=>gp.long_name + " " + name[n],'index'=>lc,'legend'=>false,'annotate'=>false)
-      DCL.sgtxzv(vx+0.05,vy,name[n],0.015,0,-1,3)
-      DCL::sgplzv([vx,vx+0.04],[vy,vy],1,lc)
-    else
-      lc = lc + 10
-      vy = vy - 0.025
-      GGraph.line(gh, false,'index'=>lc)
-      DCL.sgtxzv(vx+0.05,vy,name[n],0.015,0,-1,3)
-      DCL::sgplzv([vx,vx+0.04],[vy,vy],1,lc)
-    end
+def hist_fig(data_name,list,opt={})
+  list.dir.each_index do |n|
+    gp = gpopen list.dir[n] + data_name+".nc"
+    next if gp.nil?
+    gp = gp.wm2mmhr if gp.name.include? "Rain"
+    gp = gp.cut("sig"=>1) if gp.axnames.include? "sig"
+    gp = local_time(gp.cut("lat"=>0),hr_in_day)
+    gh = histogram_lon(gp,opt)
+    draw_fig(gh,list.name[n])
   end
 end
 
-def histogram_div(gphys,min,max,nbins)
-  ntime = gphys.coord('time').length
-  range={'min'=>min, 'max'=>max, 'nbins'=>nbins}
-  gh = 0
+def draw_fig(gh,legend,hash={})
+  xcoord = gp.axis(0).to_gphys.val
+  xmax = (xcoord[1]-xcoord[0])*xcoord.length
+  GGraph.set_axes("xlabelint"=>xmax/4,'xside'=>'bt', 'yside'=>'lr')
+  fig_opt = {'title'=>gp.long_name + " " + legend,
+    'annotate'=>false,
+    'color_bar'=>true}.merge(hash)
+  GGraph.set_fig('window'=>[0,xmax,nil,nil])
+  GGraph.tone gh,true,fig_opt
+end
 
+def histogram_dev_time(gphys,range)
+  gh = 0
   GPhys.each_along_dims(gphys,"time") do |gp|
     gh = gh + gp.histogram(range)
   end
-
   gh = gh/gphys.total
   return gh
 end
 
-def histogram_div_rain(gphys,min,max,nbins)
-  ntime = gphys.coord('time').length
-  range={'min'=>min, 'max'=>max, 'nbins'=>nbins}
-  gh = 0
-
-  GPhys.each_along_dims(gphys,"time"){|gp|
-    gh = gh + gp.histogram(range)
-  }
-
-  gh = gh/gphys.total
-  return gh
+def histogram_lon(gphys,range)
+  puts "Array size is too large" if gphys[0,false].length > 100000000
+  gh = []
+  GPhys.each_along_dims(gphys,"lon") do |gp|
+    gh << gp.histogram(range)/gp.total
+  end
+  result = GPhys.join(gh)
+  return result
 end
 
-# 
-list=ARGV[0]
-dir, name = Utiles_spe.explist(list)
-if list != nil then
-  id_exp = list.split("/")[-1].sub(".list","")
-end
+# option
+Opt = OptCharge::OptCharge.new(ARGV)
+Opt.set
 
+list = Utiles_spe::Explist.new(ARGV[0])
+IWS = 2 if Opt.charge[:ps] || Opt.charge[:eps]
+IWS = 4 if Opt.charge[:png]
+IWS = 1 if !defined? IWS
 
-if ARGV.index("-ps")
-  DCL.gropn(2)
-elsif ARGV.index("-png")
-  DCL::swlset('lwnd',false)
-  DCL.gropn(4)
-else
-  DCL.gropn(1)
-end
+clrmp = 14  # カラーマップ
+DCL::swlset('lwnd',false) if IWS==4
+DCL.sgscmn(clrmp)
+DCL.gropn(IWS)
+#DCL.sldiv('Y',2,1)
 DCL.sgpset('lcntl',true)
 DCL.sgpset('isub', 96)
-DCL.uzfact(0.9)
+DCL.uzfact(1.0)
 
-make_hist_fig("RH",0,120,120,dir,name)
+hist_fig("H2OLiqIntP",list,"min"=>0,"max"=>30,"nbins"=>300)
+#hist_fig("RH",list,"min"=>0,"max"=>120,"nbins"=>120)
 
 DCL.grcls
-
-if ARGV.index("-ps") 
-  system("mv dcl.ps #{id_exp}_hist.ps")
-elsif ARGV.index("-png")
-  system("rename 's/dcl_/#{id_exp}_hist_/' dcl_*.png")
-end
+rename_img_file(list.id,__FILE__)
