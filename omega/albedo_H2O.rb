ffd50d6808522b6ce5e1044344a7a90d36e2b0aa
ISHIOKA Sho <eee.viajero730@gmail.com>
2014-01-08 23:59:30 +0900

    Tweak

--- omega/albedo_H2O.rb
+++ omega/albedo_H2O.rb
@@ -13,27 +13,29 @@ include Math
 
 
 def draw_scatter(dir,name,hash={})
-  albedo = gpopen( dir+"local_Albedo.nc","Albedo").cut("lat"=>-30..30)
-  h2o = gpopen( dir+"H2OLiqIntP.nc").cut("lat"=>-30..30)
+  albedo = gpopen( dir+"local_Albedo.nc","Albedo").cut_rank_conserving("lat"=>0)
+  h2o = gpopen( dir+"H2OLiqIntP.nc").cut_rank_conserving("lat"=>0)
   return if albedo.nil? or h2o.nil?
-
 
   if defined?(HrInDay) and !HrInDay.nil? then
     hr_in_day = HrInDay
   else
     hr_in_day = 24 / Utiles_spe.omega_ratio(name)
   end
-  nlon = albedo.axis(0).length
+
+  nlon = h2o.axis(0).length
   
-  skip = 6*24*8
+  skip = 6*24
   (albedo.axis("time").length/skip).times{ |t|
     time = t*skip 
+
     h = h2o[false,time..time]
     h = h/cos_ang(h,hr_in_day)
     x_coord = local_time(h/cos_ang(h,hr_in_day),hr_in_day)
     x_coord = x_coord[nlon/4+1..nlon*3/4-2,false]
 #    y_coord = albedo[nlon/4+1..nlon*3/4-2,true,time..time]
     y_coord = albedo[false,time..time]
+
     hash = {'title'=> "Albedo & H2O"+" "+name,
                  'annotate'=>false,"type"=>1 }.merge(hash)
     if t == 0 then
@@ -44,50 +46,15 @@ def draw_scatter(dir,name,hash={})
   }
 end
 
-def cos_ang(gp,hr_in_day) 
-  # 太陽天頂角の計算
-  time = gp.axis("time").to_gphys
-  lon = gp.axis("lon").to_gphys if gp.axnames.include?("lon")
-  lat = gp.axis("lat").to_gphys if gp.axnames.include?("lat")
-  # 太陽直下点の計算
-  time = Utiles_spe.min2day(gp,hr_in_day).axis("time").to_gphys
-  slon = (time - time.to_i)*360
-  slon = UNumeric[slon[0].val,"degree"]    # 太陽直下点経度
-  slon = UNumeric[0,"degree"]    # 太陽直下点経度
-
-  ang = gp[false,0].copy
-  ang[false] = 1.0
-  ang.units = "1"
-  ang = ang*((ang.axis("lon").to_gphys+slon)*PI/180.0).cos
-  ang = ang*(ang.axis("lat").to_gphys*PI/180.0).cos
-  return ang + 1e-14
-end
-
-
-opt = OptionParser.new
-opt.on("-r","--rank") {Flag_rank = true}
-opt.on("-h VAL","--hr_in_day=VAL") {|hr_in_day| HrInDay = hr_in_day.to_i}
-opt.on("--ps") { IWS = 2}
-opt.on("--png") { 
-  DCL::swlset('lwnd',false)
-  IWS = 4
-}
-opt.parse!(ARGV)
-
-IWS = 1 if !defined?(IWS)
-# DCL set
-clrmp = 14  # カラーマップ
-DCL.sgscmn(clrmp)
-DCL.gropn(IWS)
-#DCL.sldiv('Y',2,1)
-DCL.sgpset('lcntl',true)
-DCL.sgpset('isub', 96)
-DCL.uzfact(1.0)
-
-
+# option
+Opt = OptCharge::OptCharge.new(ARGV)
+Opt.set
 list = Utiles_spe::Explist.new(ARGV[0])
+IWS = get_iws
+set_dcl(14)
+
 HrInDay = 24 if list.id.include?("coriolis")
-GGraph.set_fig('window'=>[0,200,0,1])
+GGraph.set_fig('window'=>[0,2,0,1])
 list.dir.each_index{ |n| draw_scatter(list.dir[n],list.name[n])}
 
 DCL.grcls
