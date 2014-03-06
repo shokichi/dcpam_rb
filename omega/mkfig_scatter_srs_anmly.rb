83927b6cd1c039952845a1532bf4b4ff5d1245d4
ISHIOKA Sho <eee.viajero730@gmail.com>
2014-03-06 22:29:23 +0900

    Rewrite

--- omega/mkfig_scatter_srs_anmly.rb
+++ omega/mkfig_scatter_srs_anmly.rb
@@ -15,16 +15,11 @@ include Math
 
 
 def fig_scat_anml(var_name,lists,hash={})
-  all = Omega::Anomaly.new(var_name,lists[:all])
-  all2 = Omega::Anomaly.new(var_name,lists[:all])
-  diurnal = Omega::Anomaly.new(var_name,lists[:diurnal])
-  coriolis = Omega::Anomaly.new(var_name,lists[:coriolis])
-  plus_dc = diurnal.plus(coriolis)
-  del_adc = all2.minus(plus_dc)
-
-  scat(all,diurnal,coriolis,lists[:all],hash)
-#  scat2(all,diurnal,lists[:diurnal],{"add"=>"A vs D "}.merge(hash))
-#  scat2(all,coriolis,lists[:coriolis],{"add"=>"A vs C "}.merge(hash))
+  all = gpaopen(var_name,lists[:all]).anomaly.delete(lists[:all].ref)
+  diurnal = gpaopen(var_name,lists[:diurnal]).anomaly.delete(lists[:diurnal].ref)
+  coriolis = gpaopen(var_name,lists[:coriolis]).anomaly.delete(lists[:coriolis].ref)
+  scat2(all,coriolis,hash)
+  scat2(all,diurnal,hash)
 #  scat2(all,plus_dc,lists[:diurnal],{"add"=>"A vs C+D "}.merge(hash))
 #  scat2(all,del_adc,lists[:all],{"add"=>"A vs A-D-C "}.merge(hash))
 end
@@ -56,59 +51,42 @@ def scat(all,diurnal,coriolis,list,hash={})
 end
 
 
-def scat2(gpa1,gpa2,list,hash={})
-    min = gpa1.anomaly[0].min
+def scat2(gpa1,gpa2,hash={})
+#    min = gpa1.min
     #   min = gpa1.anomaly[0].min
-    GGraph.set_fig('window'=>[min,-min,min,-min])
+#    GGraph.set_fig('window'=>[min,-min,min,-min])
     GGraph.set_fig('window'=>[-200,300,-200,300])
 
-  list.name.each_index do |n|
-    gpx = gpa1.anomaly[n]
-    n2 = gpa2.legend.index(gpa1.legend[n])
-    next if n2.nil?
-    gpy = gpa2.anomaly[n2]
-
-    if hash["add"]
-      addtitle = hash["add"]
-      hash.delete("add")
-    else
-      addtitle = ""
-    end
-    fig_opt = {'title'=>addtitle + gpx.long_name + " " + list.name[n],
+  n = 0
+  gpa2.list.name.each do |legend|
+    gpx = gpa1[legend]
+    gpy = gpa2[legend]
+    next if gpy.nil?
+    fig_opt = {'title'=>gpx.long_name + " " + legend,
         'annotate'=>false}.merge(hash)
     GGraph.scatter -gpx, -gpy,true,fig_opt
+    print_identifier(n)
+    n += 1
   end
 end
 
-opt = OptionParser.new
-opt.on("-r","--rank") {Flag_rank = true}
-opt.on("--ps") { IWS = 2}
-opt.on("--png") { 
-  DCL::swlset('lwnd',false)
-  IWS = 4
-}
-opt.parse!(ARGV)
-
+Opt = OptCharge::OptCharge.new
+Opt.set
 # DCL set
-IWS = 1 if !defined?(IWS)
-clrmp = 14  # カラーマップ
-DCL.sgscmn(clrmp)
-DCL.gropn(IWS)
-#DCL.sldiv('Y',2,1)
-DCL.sgpset('lcntl',true)
-DCL.sgpset('isub', 96)
-DCL.uzfact(0.8)
+IWS = get_iws
+set_dcl
 
 a_list = "/home/ishioka/link/all/fig/list/omega_all_MTlocal.list"
 d_list = "/home/ishioka/link/diurnal/fig/list/omega_diurnal_MTlocal.list"
 c_list = "/home/ishioka/link/coriolis/fig/list/omega_coriolis_MTlocal.list"
+
 lists={
   all:      Utiles_spe::Explist.new(a_list),
   diurnal:  Utiles_spe::Explist.new(d_list),
   coriolis: Utiles_spe::Explist.new(c_list)
 }
 
-fig_scat_anml("OSRA",lists)
+fig_scat_anml("OSRA",lists,set_figopt)
 
 DCL.grcls
 rename_img_file("omega",__FILE__)
